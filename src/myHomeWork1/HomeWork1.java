3b841037e8571f60d56595f35d2b0f0caebd1a94
khadiza kamal <adnanshams755@gmail.com>
2018-11-26 10:58:39 -0600

    first homework

---
+++ src/myHomeWork1/HomeWork1.java
@@ -0,0 +1,15 @@
+package myHomeWork1;
+
+public class HomeWork1 {
+
+	public static void main(String[] args) {
+		String Name = "Adnan S Shams";
+		String Address = "145-06 107th Ave, Jamaica, NY-11435";
+		
+		System.out.println(Name);
+		System.out.println(Address);
+		
+
+	}
+
+}
